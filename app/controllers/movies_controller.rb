2e174ed6343a6d748b8ac588706d676cddeee012
Fanchao Zhou <fanchaozhou1992@gmail.com>
2016-10-19 02:07:00 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,61 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @all_ratings = []
+    selection_args = []         #The 'where' clause
+    movies_each_rating = Movie.select(:rating).distinct
+    needs_redirection = false
+    
+    if params[ :ratings ] && params[ :ratings ].size>0
+      session[ :ratings ] = params[ :ratings ]
+    elsif session[ :ratings ]   #Only when we need to fill in the incomplete inputs with session data will we need redirection
+      needs_redirection = true
+    end
+    if params[ :order_by ]
+      session[ :order_by ] = params[ :order_by ]
+    elsif session[ :order_by ]  #Only when we need to fill in the incomplete inputs with session data will we need redirection
+      needs_redirection = true
+    end
+    
+    if session[ :ratings ]==nil
+      movies_each_rating.each { |m|
+        @all_ratings << {:r => m.rating, :checked => true}
+        selection_args << m.rating
+      }
+    else
+      movies_each_rating.each { |m|
+        @all_ratings << {:r => m.rating, :checked => session[ :ratings ][ m.rating ]!=nil}
+        if session[ :ratings ][ m.rating ]
+          selection_args << m.rating
+        end
+      }
+    end
+
+    if session[ :order_by ] == nil
+      @movies = Movie.where({rating: selection_args})
+      @css_title_class = []
+      @css_release_date_class = []
+    else
+      if session[ :order_by ] == "title"
+        @movies = Movie.where({rating: selection_args}).order(:title)
+        @css_title_class = [ "hilite" ]
+        @css_release_date_class = []
+      else
+        @movies = Movie.where({rating: selection_args}).order(:release_date)
+        @css_title_class = []
+        @css_release_date_class = [ "hilite" ]
+      end
+    end
+    
+    if needs_redirection
+      ratings_and_order_by = {}
+      session[ :ratings ].each { |k, v|
+        ratings_and_order_by[ "ratings[#{k}]" ] = v
+      }
+      ratings_and_order_by[ :order_by ] = session[ :order_by ]
+      flash.keep(:notice)
+      redirect_to movies_path(ratings_and_order_by)
+    end
   end
 
   def new
